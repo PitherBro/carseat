39c0f71b6acbb18c79a20a46a7527507c9ef71ab
helderma <helderma@nsuok.edu>
2020-02-25 01:44:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 var app = express();
 app.use(express.static("public"));
 //app.get("/",express.static("public"));
-app.listen(5000, ()=>{
+app.listen(/*5000, ()=>{
     console.log("now listening on 5000");
-});
+}*/);
